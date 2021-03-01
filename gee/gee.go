e16c041cb69552ef5aa00aa84e0e234d6fc78d8f
deerwang <deerwang@tencent.com>
2021-03-01 20:32:29 +0800

    part4 路由分组

--- gee/gee.go
+++ gee/gee.go
@@ -8,25 +8,51 @@ import (
 //type HandleFunc func(w http.ResponseWriter, r *http.Request)
 type HandleFunc func(c *Context)
 
+type RouterGroup struct {
+	prefix	 	string
+	middlewares []HandleFunc
+	parent 		*RouterGroup
+	engine 		*Engine
+}
+
 type Engine struct {
+	*RouterGroup
 	router *router
+	groups []*RouterGroup  // store all groups
 }
 
 func New() *Engine {
-	return &Engine{router: newRouter()}
+	engine := &Engine{router: newRouter()}
+	engine.RouterGroup = &RouterGroup{engine: engine}
+	engine.groups = []*RouterGroup{engine.RouterGroup}
+	return engine
 }
 
-func (engine *Engine) addRoute(method string, pattern string, handler  HandleFunc) {
+//r := gee.New()
+//v1 := r.Group("/v1")
+func (group *RouterGroup)Group(prefix string) *RouterGroup{
+	engine := group.engine
+	newGroup := &RouterGroup{
+		prefix: group.prefix + prefix,
+		parent: group,
+		engine: engine,
+	}
+	engine.groups = append(engine.groups, newGroup)
+	return newGroup
+}
+
+func (group *RouterGroup) addRoute(method string, comp string, handler  HandleFunc) {
+	pattern := group.prefix + comp
 	log.Printf("Route %4s - %s", method, pattern)
-	engine.router.addRoute(method, pattern, handler)
+	group.engine.router.addRoute(method, pattern, handler)
 }
 
-func (engine *Engine) GET(pattern string, handler  HandleFunc) {
-	engine.addRoute("GET", pattern, handler)
+func (group *RouterGroup) GET(pattern string, handler  HandleFunc) {
+	group.addRoute("GET", pattern, handler)
 }
 
-func (engine *Engine) POST(pattern string, handler  HandleFunc) {
-	engine.addRoute("POST", pattern, handler)
+func (group *RouterGroup) POST(pattern string, handler  HandleFunc) {
+	group.addRoute("POST", pattern, handler)
 }
 
 func (engine *Engine) Run(addr string) (err error) {
